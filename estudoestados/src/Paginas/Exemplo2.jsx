5b329a25260864aaebe83bf320418ee4a1d00e14
Fernandareis77 <fernandaoliveirareis8@gmail.com>
2025-09-02 12:10:07 -0300

    01/09

---
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -0,0 +1,39 @@
+export default function Exemplo2() {
+  return (
+    <div>
+      <h1> Exemplo 2 </h1>
+
+      <div className="conteudo">
+
+        <h3> Calculo da Média </h3>
+
+        <p>
+          O Objetivo será receber duas notas, calcular a média e 
+          exibir o resultado na tela.
+        </p>
+
+        <form>
+          
+          <p>
+            Digite o primera nota<br />
+            <input type="text" />
+          </p>
+
+          <p>
+            Digite o segunda nota <br />
+            <input type="text" />
+          </p>
+
+          <p>
+            <input type="button" value="Calcular Média" />
+          </p>
+
+        </form>
+        
+
+      </div>
+
+      
+    </div>
+  );
+}
